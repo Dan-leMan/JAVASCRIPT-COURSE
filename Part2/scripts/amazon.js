61148a56ce3965432e8bab2d71796a031e7bebe7
Dan <223422072+Dan-leMan@users.noreply.github.com>
2025-11-19 21:13:15 +0000

    Make the cart quantity interactive

--- Part2/scripts/amazon.js
+++ Part2/scripts/amazon.js
@@ -72,7 +72,15 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       productId : productId,
       quantity: 1
     });
-    };
+    }
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+    console.log(cartQuantity);
     console.log(cart);
 
   });
