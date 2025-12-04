272bd36e826135f0ae270c132e3f96f17a06955a
Dan <223422072+Dan-leMan@users.noreply.github.com>
2025-12-04 16:10:09 +0000

    added practice code to product.js

--- Part2/data/products.js
+++ Part2/data/products.js
@@ -74,6 +74,35 @@ const tshirt = new Clothing({
 console.log(tshirt);
 console.log(tshirt.getPrice());
 
+/*
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
+*/
+/*
+console.log(this);
+const object2 = {
+  a: 2,
+  b: this.a //doesn't work
+}
+*/
+/*
+
+function logThis(){
+  console.log(this);
+}
+logThis();
+
+logThis.call('hello');
+
+this
+const object3 = {
+  method: () => {
+    console.log(this);
+  }
+};
+object3.method();
+/*
 
 export const products = [
   {
